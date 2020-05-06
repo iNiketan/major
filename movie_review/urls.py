841653b89a95e1b57999cee9e51fe6f0cc1474b3
niketan <niketanip2612@gmail.com>
2020-05-06 17:43:45 +0530

    just started

--- movie_review/urls.py
+++ movie_review/urls.py
@@ -5,5 +5,7 @@ app_name = 'movie_review'
 
 urlpatterns = [
     path("", views.index, name='frontpage'),
+    path("thispage/", views.thispage, name='page'),
+
 
 ]
--- movie_review/views.py
+++ movie_review/views.py
@@ -1,4 +1,37 @@
+import keras
+import numpy as np
+import pandas as pd
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+import cv2
+import pickle
+import h5py
+import datetime
+import os
+
 from django.shortcuts import render
+from .mdls import working, cvcv
+from subprocess import run, PIPE
+
 
 def index(request):
     return render(request, 'homepage.html')
+
+def index_button(request):
+    working.go()
+    return render(request, 'homepage.html')
+
+def analysis(request):
+    # analysis page will be defined here
+    return render(request, 'analysis.html')
+
+def thispage(request):
+    return render(request, 'apage.html')
+
+
+def apage(request):
+    info = request.POST
+    out = run()
+
+    return render(request, 'apage.html')
